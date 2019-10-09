f93922a558a152aa24bba0b688650ec0fe068dc4
Vagelis Savvas <vagelis.savvas@gmail.com>
2019-10-09 12:41:03 +0300

    MCVE for issue #9343

--- src/main/resources/db/migration/V1__initialise_database.sql
+++ src/main/resources/db/migration/V1__initialise_database.sql
@@ -1,10 +1,19 @@
-DROP SCHEMA IF EXISTS mcve CASCADE;
+--DROP SCHEMA IF EXISTS mcve CASCADE;
 
-CREATE SCHEMA mcve;
+--CREATE SCHEMA mcve;
 
-CREATE TABLE mcve.test (
-  id    INT NOT NULL AUTO_INCREMENT,
-  value INT,
-  
-  CONSTRAINT pk_test PRIMARY KEY (id) 
+--SET SEARCH_PATH to mcve;
+
+
+CREATE TYPE my_type AS (
+  var_a text,
+  var_b int
 );
+
+CREATE OR REPLACE FUNCTION f_test_func(IN words text[], IN my_types my_type[], IN my_data my_type)
+RETURNS int LANGUAGE sql 
+AS $$
+
+	SELECT 1;
+
+$$
